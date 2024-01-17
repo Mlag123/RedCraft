562330139a1b0b57095fa32ee04ab7c3239b03a2
MLag <disw@list.ru>
2024-01-18 00:09:45 +0700

    editGitIghore

--- src/main/java/com/MLag/RedCraft/Main.java
+++ src/main/java/com/MLag/RedCraft/Main.java
@@ -18,12 +18,12 @@ import static com.MLag.RedCraft.client.GUI.GUITest.initTexrute;
 
 @Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
 public class Main {
+
     public static final String MODID = "red_crafting";
     public static final String NAME = "Red Crafting";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
-
     public static final String CLIENT = "com.MLag.RedCraft.Proxy.ClientProxy";
     public static final String COMMON = "com.MLag.RedCraft.Proxy.CommonProxy";
 
@@ -38,12 +38,14 @@ public class Main {
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        ReadingTexture readingTexture = new ReadingTexture();
 
         BlockReg.register();
-        regCraft.register();
+
         BlockReg.registerRender();
-        FurnanceRegistrarion.FurnanceRegistarion();
+
         GameRegistry.registerWorldGenerator(new GeneratorOre(), 0);
+        initTexrute(); // FIXME: 22.10.2023
 
     }
 
@@ -56,6 +58,7 @@ public class Main {
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-
+        FurnanceRegistrarion.FurnanceRegistarion();
+        regCraft.register();
     }
 }
