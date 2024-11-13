6c89ea750e84ea658e0712df64c33b5eb440abbd
Mlag123 <disw@list.ru>
2024-11-13 03:00:39 +0300

    creating core basement codding, for good coding mode.

---
+++ src/main/java/com/example/examplemod/Items/RedIngotItem.java
@@ -0,0 +1,10 @@
+package com.example.examplemod.Items;
+
+import com.example.examplemod.Materials.PropertiesItems;
+import net.minecraft.world.item.Item;
+
+public class RedIngotItem extends Item {
+    public RedIngotItem(Properties p_41383_) {
+        super(PropertiesItems.redIngot);
+    }
+}
---
+++ src/main/java/com/example/examplemod/Materials/PropertiesItems.java
@@ -0,0 +1,10 @@
+package com.example.examplemod.Materials;
+
+
+import net.minecraft.world.item.Item;
+
+public class PropertiesItems {
+
+    public static Item.Properties redIngot = new Item.Properties();
+
+}
