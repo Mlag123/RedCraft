3661dc72bb7e15ec84fc7a57d901e92ca1240d8a
MLag123 <disw@list.ru>
2025-12-27 18:16:31 +0700

    added model and texture

--- src/main/java/com/MLag/RedCraft/Blocks/Machines/FireCraftingTableBlock.java
+++ src/main/java/com/MLag/RedCraft/Blocks/Machines/FireCraftingTableBlock.java
@@ -3,11 +3,16 @@ package com.MLag.RedCraft.Blocks.Machines;
 import com.MLag.RedCraft.Blocks.BlockCreate;
 import com.MLag.RedCraft.Main;
 import com.MLag.RedCraft.Tiles.FiteTileCraftingTableBlock;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -18,8 +23,12 @@ import net.minecraft.world.World;
 import javax.annotation.Nullable;
 
 public class FireCraftingTableBlock extends BlockCreate implements ITileEntityProvider {
+    public PropertyDirection FACING = BlockHorizontal.FACING;
+
     public FireCraftingTableBlock(String name, Material material, float setHardness, CreativeTabs creativeTabs, SoundType soundType) {
         super(name, material, setHardness, creativeTabs, soundType);
+        setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+
     }
 
 
@@ -35,5 +44,35 @@ public class FireCraftingTableBlock extends BlockCreate implements ITileEntityPr
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new FiteTileCraftingTableBlock();
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+        return super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer, hand).withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {BlockHorizontal.FACING});
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(BlockHorizontal.FACING, EnumFacing.getHorizontal(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(BlockHorizontal.FACING).getHorizontalIndex()&1;
     }
 }
